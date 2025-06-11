1767afa39b90517eb504de9150545fcdca6e01ac
Yan <yan.malyshev.2002@gmail.com>
2025-06-11 19:18:40 +0300

    calculator app

---
+++ math_func.h
@@ -0,0 +1,18 @@
+#ifndef MATH_FUNC_H
+#define MATH_FUNC_H
+namespace math{
+	double plus(double first_n, double second_n);
+
+	double minus(double first_n, double second_n);
+
+	double multiply(double first_n, double second_n);
+
+	double devide(double first_n, double second_n);
+
+	double pow(double first_n, int power_n);
+
+	double factorial(int first_n);
+		
+} // namespace math
+
+#endif // MATH_FUNC_H
